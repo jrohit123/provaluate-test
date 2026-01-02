49387b069eda9dad02b4ad9f642dfd259e846fa7
Kartik Batchu <kartikvaids121@siesgst.ac.in>
2026-01-02 16:48:38 +0530

    new changes

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,12 @@ import App from './App';
 import './index.css';
 
 // 🔎 Debug: log env values at startup
-console.log('ENV', {
+/*console.log('ENV', {
   VITE_SUPABASE_URL: import.meta.env.VITE_SUPABASE_URL,
   VITE_SUPABASE_ANON_KEY: import.meta.env.VITE_SUPABASE_ANON_KEY,
   VITE_API_BASE_URL: import.meta.env.VITE_API_BASE_URL,
   VITE_PYTHON_URL: import.meta.env.VITE_PYTHON_URL,
-});
+});*/
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
