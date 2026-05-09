fa2410cd38b30fb0497884cd8cefcca61320e10a
Kartik Batchu <kartikvaids121@siesgst.ac.in>
2026-05-09 09:18:14 +0530

    Assessment Handling

--- vite.config.ts
+++ vite.config.ts
@@ -33,6 +33,7 @@ export default defineConfig(({ mode }) => {
       legalComments: 'none',
     },
     build: {
+      target: 'esnext',
       sourcemap: false,
       minify: false,
       chunkSizeWarningLimit: 1000,
